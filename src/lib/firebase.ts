d07c218a75da2c5a92c911e3915e8fdcc1b00de0
Anuraj Rajput <rajputanuraj1@gmail.com>
2023-10-31 16:30:16 +0530

    second upload of src

---
+++ src/lib/firebase.ts
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "potion-a6f42.firebaseapp.com",
+  projectId: "potion-a6f42",
+  storageBucket: "potion-a6f42.appspot.com",
+  messagingSenderId: "504977022291",
+  appId: "1:504977022291:web:9bf2e4fbfe250ef1b2d5a6",
+  measurementId: "G-RC5LCPT9VP"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
